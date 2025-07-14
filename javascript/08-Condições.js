a9f74bab43b095a30d6cc3f9bae817d61507d1f2
Luiz <jc381311@gmail.com>
2025-07-13 23:26:12 -0300

    add

---
+++ javascript/08-Condições.js
@@ -0,0 +1,9 @@
+let vel = 50;
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+if (vel > 60) {
+  console.log("Multado! Você ultrapassou o limite de velocidade permitida.");
+} else {
+  console.log("Parabéns! Você está andando dentro do limite de velocidade.");
+}
+console.log("Dirija sempre com cuidado!");
+ 
---
+++ javascript/09-Condições.js
@@ -0,0 +1,6 @@
+let pais = "Alemanha";
+if (pais != "Brasil") {
+  console.log("Você é Estrangeiro!");
+} else {
+  console.log("Você é Brasileiro!");
+}
